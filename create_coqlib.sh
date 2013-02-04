9a0b6aba0ad5fb1bfa3d8f6f10b9d4fab23e3ade
Keigo IMAI <keigo.imai@gmail.com>
2013-02-04 19:32:38 +0900

    generate bytecode file using 'ocamlc -noautolink -use-prims'

--- create_coqlib.sh
+++ create_coqlib.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+set -e
+set -v
 
 ##############################################################
 ## Generate Coq bytecode (requires our custom coqmktop -a)
 ##############################################################
-coqmktop -a -no-start -o coqlib.cma
-
+coqmktop -a -no-start -I `coqtop -where`/parsing main.ml -o coqlib.cma
+./prepare_ocaml.sh
+ocamlc -I `coqtop -where` -noautolink -use-prims tmp/primitives-all coqlib.cma -o coqlib.byte
