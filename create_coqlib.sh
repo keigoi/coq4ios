5c519b7ab616cd14c0cf82d9255138f50e2db7bc
Keigo IMAI <keigo.imai@gmail.com>
2013-02-09 01:15:00 +0900

    compile *.v files on iOS (OCaml part)

--- create_coqlib.sh
+++ create_coqlib.sh
@@ -1,27 +1,40 @@
 #!/bin/sh
+set -e
+set -v
 cd `dirname $0`
 PWD=`pwd`
 COQTOPDIR=`coqtop -where`
-set -e
-set -v
 
 ##############################################################
 ## Generate Coq bytecode (requires our custom coqmktop -a)
 ##############################################################
-coqmktop -g -a -no-start -I $COQTOPDIR/parsing main.ml -o coqlib.cma
+ocamllex coq-src/tools/coqdep_lexer.mll
+ocamlc -c -I coq-src/tools coq-src/tools/coqdep_lexer.mli coq-src/tools/coqdep_lexer.ml coq-src/tools/coqdep_common.mli coq-src/tools/coqdep_common.ml
+coqmktop -g -a -no-start -I $COQTOPDIR/kernel -I $COQTOPDIR/parsing -I $COQTOPDIR/library -I $COQTOPDIR/interp -I coq-src/tools coqdep_lexer.cmo coqdep_common.cmo main.ml -o coqlib.cma
 ./prepare_ocaml.sh
 ocamlc -g -I $COQTOPDIR -noautolink -linkall -use-prims tmp/primitives-all coqlib.cma -o coqlib.byte
+
+#######################
+## make NMake_gen.v
+#######################
+make -C coq-src theories/Numbers/Natural/BigN/NMake_gen.v
 
 #####################
 ## copy Coq dirs
 #####################
-COQLIBSRC=$COQTOPDIR
+COQLIBSRC=coq-src
 COQLIBDST=$PWD/Coq4iPad/Coq4iPad/coq-8.4pl1
+# clean
 rm -rf $COQLIBDST/plugins $COQLIBDST/states $COQLIBDST/theories
-cp -r $COQLIBSRC/plugins $COQLIBDST
-cp -r $COQLIBSRC/states $COQLIBDST
-cp -r $COQLIBSRC/theories $COQLIBDST
-#mkdir -p $COQLIBDST/theories; cp -r $COQLIBSRC/theories/Init $COQLIBSRC/theories/Logic $COQLIBSRC/theories/Arith $COQLIBDST/theories
+mkdir -p $COQLIBDST/states
+# copy plugin binaries
+pushd $COQTOPDIR
+  find plugins -type d -exec mkdir -p $COQLIBDST/{} \;
+  find theories -type d -exec mkdir -p $COQLIBDST/{} \;
+  find plugins -name '*.cm[ioa]' -exec cp {} $COQLIBDST/{} \;
+  find theories -name '*.cm[ioa]' -exec cp {} $COQLIBDST/{} \;
+popd
+# copy NMake_gen.v
+cp coq-src/theories/Numbers/Natural/BigN/NMake_gen.v $COQLIBDST/theories/Numbers/Natural/BigN/
 
-find $COQLIBDST/plugins -name '*.cmxs' |xargs rm
 
--- main.ml
+++ main.ml
@@ -29,15 +29,146 @@ let eval (str:string) =
   with
       V.End_of_input -> ()
 
-let compile (filename:string) =
-  V.compile false filename
+type mod_id = string list
+type filepath = string
 
-let start () =
-  print_endline "coq4ios: initializing...";
-  Coqtop.init_toplevel (List.tl (Array.to_list Sys.argv))
+let rec suffixes = function
+  | [] -> assert false
+  | [name] -> [[name]]
+  | dir::suffix as l -> l::suffixes suffix
+
+let all_theories roots : (mod_id, filepath) Hashtbl.t * filepath list =
+  let hash = Hashtbl.create 12 in
+  let files = ref [] in
+  let add_known physi logi fn = 
+    if not (Filename.check_suffix fn ".v") then () else 
+    if List.exists (fun n -> n="test") logi then () else
+    let basename = Filename.chop_suffix fn ".v" in
+    let fullpath = physi^"/"^fn in
+    files := fullpath :: !files; (* add filename *)
+    let suff = suffixes (logi @ [basename]) in
+    List.iter (fun n -> Hashtbl.add hash n fullpath) suff (* update mapping *)
+  in
+  List.iter (fun root -> Coqdep_common.add_rec_dir add_known root ["Coq"]) roots;
+  hash, List.rev !files
+
+let coqdep pathmap file : filepath list =
+  let module L = Coqdep_lexer in
+  let find m = try Hashtbl.find pathmap m with _ -> failwith (String.concat "." m) in
+  try
+    let chan = open_in file in
+    let buf = Lexing.from_channel chan in
+    let addQueue q v = q := v :: !q in
+    let deja_vu_v = ref ([]: filepath list)
+    (*and deja_vu_ml = ref ([] : string list)*) in
+    begin try
+      while true do
+      	let tok = L.coq_action buf in
+	match tok with
+	  | L.Require strl ->
+	      List.iter (fun str ->
+                let path = find str in
+		if not (List.mem path !deja_vu_v) then begin
+	          addQueue deja_vu_v path
+       		end) strl
+	  | L.RequireString s ->
+	      let str = Filename.basename s in
+              let path = find [str] in
+	      if not (List.mem path !deja_vu_v) then begin
+	        addQueue deja_vu_v path;
+       	      end
+	  | L.Load _ -> () (* ignore *)
+          | L.Declare _ -> () (* ignore *)
+          | L.AddLoadPath _ | L.AddRecLoadPath _ -> (* TODO *) ()
+      done
+    with L.Fin_fichier -> close_in chan
+       | L.Syntax_error (i,j) -> close_in chan; failwith (Printf.sprintf "cannot parse %s at (%d, %d)" file i j)
+    end;
+    List.rev !deja_vu_v
+  with Sys_error _ -> []
+
+let coqdep_all pathmap all_files : (filepath, filepath list) Hashtbl.t =
+  let hash = Hashtbl.create 19 in
+  List.iter (fun file -> 
+    Hashtbl.add hash file (coqdep pathmap file)) all_files;
+  hash
+
+let compile state file = 
+  let file = Filename.chop_suffix file ".v" in
+  States.unfreeze state;
+  (* Dumpglob.coqdoc_unfreeze clean_coqdoc_init_state; *)
+  V.compile false file
+
+let compile_all state files = 
+  try
+    List.iter (fun file ->
+      print_endline ("compiling: "^ file);
+      compile state file) files
+  with Util.Error_in_file (file, (_,_,loc), exn) -> 
+    Printf.printf "Compile Error: (%d, %d) in %s\n" (L.first_pos loc) (L.last_pos loc) file;
+    raise exn
+
+let toposort graph start_nodes = 
+  let rec explore check visited node = 
+    if List.mem node check then failwith (Printf.sprintf "cycle found at %s" node) else
+    if List.mem node visited then visited else     
+      let check = node :: check in 
+      let edges    = try Hashtbl.find graph node with Not_found -> failwith (Printf.sprintf "edges not found for %s" node) in
+      let visited  = List.fold_left (explore check) visited edges in
+      node :: visited
+  in 
+  let rev = List.fold_left (explore []) [] start_nodes in
+  List.rev rev
+
+let make_pathmap root = 
+  all_theories [root^"/theories"; root^"/plugins"; root^"/states"]
+
+let compile_theories () = 
+  let raw_state = States.freeze() in
+
+  (* enumerate all .v files *)
+  let pathmap, all_files = make_pathmap !Flags.coqlib in
+  print_endline "got stdlibs.";
+  (* make a dependency graph *)
+  let deps = coqdep_all pathmap all_files in
+
+  let initfile = Hashtbl.find pathmap ["MakeInitial"] in
+  let initsorted = toposort deps [initfile] in
+  compile_all raw_state initsorted;
+  
+  States.unfreeze raw_state;
+  (* Declaremods.start_library (Names.make_dirpath [Names.id_of_string "Top"]); *)
+  V.load_vernac false (!Flags.coqlib^"/states/MakeInitial.v");
+  let init_state = States.freeze() 
+  in
+
+  let all_sorted = toposort deps all_files in
+  let all_sorted = List.filter (fun f -> not (List.mem f initsorted)) all_sorted in
+  compile_all init_state all_sorted
+
+
+(* -coqlib <dir> -boot -nois -notop *)
+
+let start root =
+  Lib.init();
+  Goptions.set_string_option_value ["Default";"Proof";"Mode"] "Classic";
+
+  Flags.coqlib_spec:=true; 
+  Flags.coqlib:=root;
+  Flags.boot:=true; 
+
+  Coqinit.init_load_path ();
+  Mltop.init_known_plugins ();
+  Vm.set_transp_values true;
+  Vconv.set_use_vm false;
+  (* engage (); *)
+  Syntax_def.set_verbose_compat_notations false;
+  Syntax_def.set_compat_notations true;
+  Coqinit.init_library_roots ();
 ;;
 
 
-start ();
-Callback.register "parse" parse;
+Callback.register "start" start;
+Callback.register "compile_theories" compile_theories;
 Callback.register "eval" eval;
+Callback.register "parse" parse;
